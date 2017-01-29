011aa77e05f92e38331a4efb5f2b80f2be9d4916
Chris Handley <chrisrhandley@gmail.com>
2017-01-29 23:07:58 +0000

    kitten is clickable, use template

--- app.go
+++ app.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"io"
 	"os"
+	"html/template"
 )
 
 func WebHandler (w http.ResponseWriter, r *http.Request) {
@@ -18,8 +19,34 @@ func WebHandler (w http.ResponseWriter, r *http.Request) {
 	io.Copy(w, img)
 }
 
+func handler (w http.ResponseWriter, r *http.Request) {
+	var myInfo = struct{
+		Image string
+	}{"kitten"}
+
+	const templ = `<body><img src="http://localhost:8080/images/{{.Image}}.jpg"><p>You clicked {{.Image}}!</p><body>`
+	reports, err := template.New("report").
+		Parse(templ)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = reports.Execute(w, myInfo)
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	const text = `<html>
+	<body>
+	<a href="http://localhost:8080/clicked"><img src="http://localhost:8080/images/kitten.jpg"/></a>
+	</body>
+	</html>`
+	fmt.Fprintf(w, text)
+}
+
 func main()  {
 	fmt.Println("started")
-	http.HandleFunc("/", WebHandler)
+	http.HandleFunc("/", viewHandler)
+	http.HandleFunc("/clicked", handler)
+	http.HandleFunc("/images/", WebHandler)
 	http.ListenAndServe(":8080", nil)
 }
