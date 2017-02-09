25d86a9be2fac35d27cda7c4eb3a9031fac14b0a
Chris Handley <chrisrhandley@gmail.com>
2017-02-09 16:52:27 +0000

    can detect pairs

--- app.go
+++ app.go
@@ -5,10 +5,14 @@ import (
 	"net/http"
 	"html/template"
 	"strconv"
+	"time"
 )
 
 type cell struct  {
 	Animal string
+	Clicked bool
+	Paired bool
+	Visible bool
 }
 
 type row struct {
@@ -19,8 +23,9 @@ type board struct {
 	Rows []row
 }
 
-
-
+type server struct {
+	currentBoard *board
+}
 
 func webHandler (w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, r.URL.Path[1:])
@@ -30,14 +35,76 @@ func (s *server) clickHandler(w http.ResponseWriter, r *http.Request) {
 	locationString := r.URL.Path[len("/clicked/"):]
 	rowIndex, _ := strconv.Atoi(locationString[:1])
 	colIndex, _ := strconv.Atoi(locationString[2:])
-	fmt.Println("got a click")
-	s.currentBoard.Rows[rowIndex].Cells[colIndex] = cell{"martha"}
+	s.currentBoard.Rows[rowIndex].Cells[colIndex].Clicked = true
+	s.currentBoard.Rows[rowIndex].Cells[colIndex].Visible = !(s.currentBoard.Rows[rowIndex].Cells[colIndex].Visible)
 	http.Redirect(w, r, "/board/", http.StatusFound)
 	return
 }
 
-type server struct {
-	currentBoard *board
+func (s *server) clickedCount() (clickedCount int) {
+	for _, thisRow := range s.currentBoard.Rows {
+		for _, thisCell := range thisRow.Cells {
+			if thisCell.Clicked  {
+				clickedCount += 1
+			}
+		}
+	}
+	return
+}
+
+func (s * server) resetClicked() {
+	for i, thisRow := range s.currentBoard.Rows {
+		for j, thisCell := range thisRow.Cells {
+			if thisCell.Clicked  {
+				s.currentBoard.Rows[i].Cells[j].Clicked = false
+				if !thisCell.Paired {
+					s.currentBoard.Rows[i].Cells[j].Visible = false
+				}
+			}
+		}
+	}
+	return
+}
+
+func (s *server) resetIfTwoClicked(w http.ResponseWriter, r *http.Request) {
+	if s.clickedCount() >= 2 {
+		time.Sleep(500 * time.Millisecond)
+		s.resetClicked()
+		http.Redirect(w, r, "/board/", http.StatusFound)
+
+	}
+}
+
+func (s *server) checkForClickedPair() (bool){
+	type indexPair struct{
+		x int
+		y int
+	}
+	clickedCells := []*cell{}
+	clickedIndices := []indexPair{}
+	for i, thisRow := range s.currentBoard.Rows {
+		for j, thisCell := range thisRow.Cells {
+			if thisCell.Clicked  {
+				clickedCells = append(clickedCells, &thisCell)
+				clickedIndices = append(clickedIndices, indexPair{i,j,})
+			}
+		}
+	}
+	if len(clickedCells) != 2 {
+		return false
+	}
+
+	if clickedCells[0].Animal == clickedCells[1].Animal {
+		fmt.Println("yes pair yay")
+		s.currentBoard.Rows[clickedIndices[0].x].Cells[clickedIndices[0].y].Paired = true
+		s.currentBoard.Rows[clickedIndices[1].x].Cells[clickedIndices[1].y].Paired = true
+
+		clickedCells[0].Paired = true
+		clickedCells[1].Paired = true
+		return true
+	}
+
+	return false
 }
 
 
@@ -49,9 +116,8 @@ func (s *server) viewHandler (w http.ResponseWriter, r *http.Request) {
 	{{ range $rowIndex, $trow := .Rows }}
 		<div width="100%">
 		{{range $colIndex, $tcell := .Cells}}
-
 			<a href="http://localhost:8080/clicked/{{$rowIndex}}/{{$colIndex}}">
-			<img style="width:200px; height:200px; object-fit:cover" src="http://localhost:8080/images/{{$tcell.Animal}}.jpg">
+			<img style="width:200px; height:200px; object-fit:cover" src="http://localhost:8080/images/{{if $tcell.Visible}}{{$tcell.Animal}}.jpg{{else}}mergermarket.jpg {{end}}">
 			</a>
 		{{end}}
 		<div>
@@ -61,6 +127,12 @@ func (s *server) viewHandler (w http.ResponseWriter, r *http.Request) {
 	gameBoard := template.Must(template.New("gameBoard").Parse(templ))
 
 	gameBoard.Execute(w, s.currentBoard)
+
+	if s.clickedCount() >= 2 {
+		time.Sleep(500 * time.Millisecond)
+		s.resetClicked()
+		http.Redirect(w, r, "/board/", http.StatusFound) // TODO does not redirect!
+	}
 }
 
 func main()  {
@@ -76,25 +148,25 @@ func main()  {
 func getNewBoard() *board {
 	first := row{
 		[]cell{
-			cell{"puppy"},
-			cell{"puppy"},
-			cell{"kitten"},
+			cell{Animal: "puppy"},
+			cell{Animal: "kitten"},
+			cell{Animal: "martha"},
 		},
 	}
 
 	second := row{
 		[]cell{
-			cell{"kitten"},
-			cell{"puppy"},
-			cell{"kitten"},
+			cell{Animal: "puppy"},
+			cell{Animal: "kitten"},
+			cell{Animal: "puppy"},
 		},
 	}
 
 	third := row{
 		[]cell{
-			cell{"kitten"},
-			cell{"kitten"},
-			cell{"puppy"},
+			cell{Animal: "kitten"},
+			cell{Animal: "puppy"},
+			cell{Animal: "kitten"},
 		},
 	}
 
@@ -107,36 +179,36 @@ func getNewBoard() *board {
 	return &myboard
 }
 
-func getAllPuppies() *board {
-	first := row{
-		[]cell{
-			cell{"puppy"},
-			cell{"puppy"},
-			cell{"puppy"},
-		},
-	}
-
-	second := row{
-		[]cell{
-			cell{"puppy"},
-			cell{"puppy"},
-			cell{"puppy"},
-		},
-	}
-
-	third := row{
-		[]cell{
-			cell{"puppy"},
-			cell{"puppy"},
-			cell{"puppy"},
-		},
-	}
-
-	myboard := board{
-		[]row{
-			first, second, third,
-		},
-	}
-
-	return &myboard
-}
+//func getAllPuppies() *board {
+//	first := row{
+//		[]cell{
+//			cell{"puppy"},
+//			cell{"puppy"},
+//			cell{"puppy"},
+//		},
+//	}
+//
+//	second := row{
+//		[]cell{
+//			cell{"puppy"},
+//			cell{"puppy"},
+//			cell{"puppy"},
+//		},
+//	}
+//
+//	third := row{
+//		[]cell{
+//			cell{"puppy"},
+//			cell{"puppy"},
+//			cell{"puppy"},
+//		},
+//	}
+//
+//	myboard := board{
+//		[]row{
+//			first, second, third,
+//		},
+//	}
+//
+//	return &myboard
+//}
--- app_test.go
+++ app_test.go
@@ -1,1 +1,66 @@
 package main
+
+import "testing"
+
+func TestCountClicked(t *testing.T) {
+	s := getServerWithOneClicked()
+
+	if s.clickedCount() != 1 {
+		t.Error("should count one clicked cell")
+	}
+}
+func getServerWithOneClicked() server {
+	first := row{
+		[]cell{
+			cell{Animal: "puppy"},
+			cell{Animal: "puppy"},
+			cell{Animal: "puppy"},
+		},
+	}
+	myboard := board{
+		[]row{
+			first,
+		},
+	}
+	myboard.Rows[0].Cells[0].Clicked = true
+	s := server{&myboard}
+	return s
+}
+
+func TestResetClicked(t *testing.T) {
+	s := getServerWithOneClicked()
+
+	s.resetClicked()
+
+	if s.clickedCount() != 0 {
+		t.Error("reset clicked should reset to zero")
+	}
+}
+
+func TestDontMarkClickedNotPair(t *testing.T) {
+	s := getServerWithOneClicked()
+
+	s.currentBoard.Rows[0].Cells[1] = cell{Animal: "kitten", Clicked: true}
+	s.checkForClickedPair()
+
+	if s.currentBoard.Rows[0].Cells[1].Paired || s.currentBoard.Rows[0].Cells[0].Paired {
+		t.Error ("puppy and kitten should not be marked as pair",
+			s.currentBoard.Rows[0].Cells[1].Paired,
+			s.currentBoard.Rows[0].Cells[0].Paired)
+	}
+
+}
+
+func TestMarkClickedPair(t *testing.T) {
+	s := getServerWithOneClicked()
+
+	s.currentBoard.Rows[0].Cells[1] = cell{Animal: "puppy", Clicked: true}
+	s.checkForClickedPair()
+
+	if !(s.currentBoard.Rows[0].Cells[1].Paired && s.currentBoard.Rows[0].Cells[0].Paired) {
+		t.Error ("puppy and puppy should both be marked as pair",
+			s.currentBoard.Rows[0].Cells[1].Paired,
+			s.currentBoard.Rows[0].Cells[0].Paired)
+	}
+
+}
