9d84d5ba076cfa65c91d5457a1a524aad2409c1e
Chris Handley <chrisrhandley@gmail.com>
2017-02-09 15:40:18 +0000

    show different image on click

--- app.go
+++ app.go
@@ -3,46 +3,77 @@ package main
 import "fmt"
 import (
 	"net/http"
-	"log"
 	"html/template"
+	"strconv"
 )
+
+type cell struct  {
+	Animal string
+}
+
+type row struct {
+	Cells []cell
+}
+
+type board struct {
+	Rows []row
+}
+
+
+
 
 func webHandler (w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, r.URL.Path[1:])
 }
 
-func handler (w http.ResponseWriter, r *http.Request) {
-	var myInfo = struct{
-		Image string
-	}{"kitten"}
-
-	const templ = `<body><img src="http://localhost:8080/images/{{.Image}}.jpg"><p>You clicked {{.Image}}!</p><body>`
-	reports, err := template.New("report").
-		Parse(templ)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = reports.Execute(w, myInfo)
+func (s *server) clickHandler(w http.ResponseWriter, r *http.Request) {
+	locationString := r.URL.Path[len("/clicked/"):]
+	rowIndex, _ := strconv.Atoi(locationString[:1])
+	colIndex, _ := strconv.Atoi(locationString[2:])
+	fmt.Println("got a click")
+	s.currentBoard.Rows[rowIndex].Cells[colIndex] = cell{"martha"}
+	http.Redirect(w, r, "/board/", http.StatusFound)
+	return
 }
 
-func viewHandler(w http.ResponseWriter, r *http.Request) {
+type server struct {
+	currentBoard *board
+}
 
-	type cell struct  {
-		Animal string
-	}
 
-	type row struct {
-		Cells []cell
-	}
 
-	type board struct {
-		Rows []row
-	}
+func (s *server) viewHandler (w http.ResponseWriter, r *http.Request) {
+	fmt.Println("showing board")
+	const templ = `
+	<body>
+	{{ range $rowIndex, $trow := .Rows }}
+		<div width="100%">
+		{{range $colIndex, $tcell := .Cells}}
 
-	//puppy := cell{"puppy"}
-	//kitten := cell{"kitten"}
+			<a href="http://localhost:8080/clicked/{{$rowIndex}}/{{$colIndex}}">
+			<img style="width:200px; height:200px; object-fit:cover" src="http://localhost:8080/images/{{$tcell.Animal}}.jpg">
+			</a>
+		{{end}}
+		<div>
+	{{ end }}
+	<body>`
 
+	gameBoard := template.Must(template.New("gameBoard").Parse(templ))
+
+	gameBoard.Execute(w, s.currentBoard)
+}
+
+func main()  {
+	myBoard := getNewBoard()
+	server := &server{myBoard}
+	fmt.Println("started")
+	http.HandleFunc("/board/", server.viewHandler)
+	http.HandleFunc("/clicked/", server.clickHandler)
+	http.HandleFunc("/images/", webHandler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func getNewBoard() *board {
 	first := row{
 		[]cell{
 			cell{"puppy"},
@@ -67,48 +98,45 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 		},
 	}
 
-	pets := board{
+	myboard := board{
 		[]row{
 			first, second, third,
 		},
 	}
 
-
-	const templ = `<body>
-
-	{{ range $rowIndex, $trow := .Rows }}
-		<div width="100%">
-
-		{{range $colIndex, $tcell := .Cells}}
-
-			<a href="http://localhost:8080/clicked/{{$rowIndex}}/{{$colIndex}}"><img style="width:200px;height:200px;object-fit: cover" src="http://localhost:8080/images/{{$tcell.Animal}}.jpg"></a>
-
-		{{end}}
-
-		<div>
-
-	{{ end }}
-
-
-	<body>`
-
-	const cellTemplate = `<a href="http://localhost:8080/clicked"><img src="http://localhost:8080/images/{{.Animal}}.jpg"></a>`
-
-	var loopFunc = func(n int) []struct{} {
-		return make([]struct{}, n)
-	}
-
-	reports := template.Must(template.New("report").Funcs(template.FuncMap{
-		"loop": loopFunc,
-	}).Parse(templ))
-
-	reports.Execute(w, pets)
+	return &myboard
 }
 
-func main()  {
-	fmt.Println("started")
-	http.HandleFunc("/", viewHandler)
-	http.HandleFunc("/clicked", handler)
-	http.HandleFunc("/images/", webHandler)
-	http.ListenAndServe(":8080", nil)
+func getAllPuppies() *board {
+	first := row{
+		[]cell{
+			cell{"puppy"},
+			cell{"puppy"},
+			cell{"puppy"},
+		},
+	}
+
+	second := row{
+		[]cell{
+			cell{"puppy"},
+			cell{"puppy"},
+			cell{"puppy"},
+		},
+	}
+
+	third := row{
+		[]cell{
+			cell{"puppy"},
+			cell{"puppy"},
+			cell{"puppy"},
+		},
+	}
+
+	myboard := board{
+		[]row{
+			first, second, third,
+		},
+	}
+
+	return &myboard
 }
